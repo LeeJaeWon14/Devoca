f4e2a97c2c94285e6035ef98105719bbdffe21cc
LeeJaeWon <ghkdwpqndtls@naver.com>
2023-01-13 15:25:40 +0900

    - commit..

--- app/src/main/java/com/jeepchief/devoca/MainActivity.kt
+++ app/src/main/java/com/jeepchief/devoca/MainActivity.kt
@@ -2,10 +2,28 @@ package com.jeepchief.devoca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.jeepchief.devoca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    private fun initUI() {
+        binding.apply {
+            btnVoca.setOnClickListener {
+
+            }
+
+            btnSetting.setOnClickListener {
+
+            }
+        }
     }
 }
